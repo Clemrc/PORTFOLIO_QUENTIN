51a3ace8a4300193338f9c9f6c519e76e44bb7c6
Clément MAURICE <clemaurice77@gmail.com>
2020-06-28 21:57:10 +0200

    add pages

---
+++ pages/contact.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="content">
+        <div class="title-content">
+          <h1 class="title">Contact</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  transition: 'slide-fade',
+  head() {
+    return {
+      title: 'Quentin Demaria - contact',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'portfolio de Quentin Demaria'
+        },
+        {
+          name: 'keyword',
+          content: 'Quentin Demaria, Quentin Demaria portfolio'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.content {
+  display: flex;
+  justify-content: flex-start;
+  align-items: baseline;
+  flex-direction: column;
+  /* height: 2000px; */
+  width: 100%;
+  /* border: 1px solid black; */
+}
+
+.title-content {
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-end;
+}
+
+.projects-content {
+  margin: 10px;
+  width: 20px;
+  height: 20px;
+  background-color: black;
+}
+
+.title {
+  font-weight: 400;
+  font-size: 80px;
+  color: black;
+  margin-bottom: 0;
+}
+
+.dark .title {
+  color: white;
+  transition: 0.5s;
+}
+
+.square {
+  width: 13px;
+  height: 13px;
+  background-color: black;
+  margin-left: 14px;
+  margin-bottom: 20px;
+}
+</style>
